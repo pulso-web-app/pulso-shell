1e9670189aabed13e8288aa681fca029c59cd051
Ewerton Mendes <ewertonmendes9@gmail.com>
2026-08-20 17:20:59 -0300

    test: add router provider to NavigationComponent tests

--- apps/shell/src/app/layout/navigation/navigation.component.spec.ts
+++ apps/shell/src/app/layout/navigation/navigation.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { NavigationComponent } from './navigation.component';
 
 describe('NavigationComponent', () => {
@@ -8,6 +9,7 @@ describe('NavigationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavigationComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavigationComponent);
